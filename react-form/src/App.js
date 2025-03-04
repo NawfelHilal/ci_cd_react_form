aea840907529636dc63c76f69aad1a7b335971f1
NawfelHilal <nawfelhilal13@gmail.com>
2025-03-04 12:34:34 +0100

    fix warning

--- react-form/src/App.js
+++ react-form/src/App.js
@@ -1,13 +1,9 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Form from "./components/Form";
 
 function App() {
   let [count, setCount] = React.useState(0);
-  const clickOnMe = () => {
-    setCount(count + 1);
-  };
 
   return (
     <div className="App">
