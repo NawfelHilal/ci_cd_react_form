5e0fb0b7299166896a9764037e0d219a6c3e2f31
NawfelHilal <nawfelhilal13@gmail.com>
2025-06-16 12:10:57 +0200

    fix bug 8

--- react-form/cypress/e2e/spec.cy.js
+++ react-form/cypress/e2e/spec.cy.js
@@ -2,6 +2,6 @@ describe("Home page spec", () => {
   it("deployed react app to localhost", () => {
     cy.visit("http://localhost:3000");
     cy.wait(1000);
-    cy.contains("Nombre d'utilisateurs : 1");
+    cy.contains("Nombre d'utilisateurs : 2");
   });
 });
